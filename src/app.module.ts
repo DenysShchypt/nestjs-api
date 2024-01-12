bf55449b8a3ee25d85621adbb492bb89128493c2
DenysShchypt <denysshchyptsovit@gmail.com>
2024-01-12 18:54:11 +0200

    create folder modules with entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactsModule } from './modules/contacts/contacts.module';
+import { Contact } from './modules/contacts/entities/contact.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'rest-api',
+      entities: [Contact],
+      synchronize: true,
+    }),ContactsModule
+  ],
+  controllers:[AppController],
+  providers:[AppService]
 })
 export class AppModule {}
---
+++ src/modules/contacts/contacts.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contact } from './entities/contact.entity';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Contact])
+  ],
+  controllers:[],
+  providers:[]
+})
+export class ContactsModule {}
